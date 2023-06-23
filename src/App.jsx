3bd3c10f0ea38705ce84d3e06c49adef7706b696
Adamya Singh <adamya45@gmail.com>
2023-06-23 19:18:16 +0530

    scaffold the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
-import 'rsuite/dist/styles/rsuite-default.css';
+import React from "react";
+import 'rsuite/dist/rsuite.min.css';
 import './styles/main.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/signin">SignIn</Route>
-        <Route path="/">Home</Route>
-      </Switch>
-    </BrowserRouter>
+    <div>hello</div>
   );
 }
 
