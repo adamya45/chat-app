d209c15456b1b6361fbbab74682031e4167e64a3
Adamya Singh <adamya45@gmail.com>
2023-07-03 19:20:54 +0530

    linked facebook and google to one account, created provider black

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Button, Divider, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
-import EditableInput from './EditableInput';
+import EditableInput from '../EditableInput';
+import { database } from '../../misc/firebase';
+import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({onSignOut}) => {
 
   const { profile } = useProfile();
   const onSave = async newData => {
 
-    console.log(newData);
+    const userNicknaeRef = database.ref(`/profiles/${profile.uid}`).child('name');
+
+    try {
+      await userNicknaeRef.set(newData);
+
+      Alert.success('Nickname has been updated', 4000);
+
+    } catch (err) {
+      Alert.error(err.message, 4000);      
+    }
+
   }
 
   return (
@@ -21,6 +33,7 @@ const Dashboard = ({onSignOut}) => {
 
     <Drawer.Body>
       <h3>Hello, {profile.name}</h3>
+      <ProviderBlock />
       <Divider />
       <EditableInput
         name="nicknake"
