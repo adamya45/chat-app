acc8db923c0588d7c22badaf90ae93b87d7acaa3
Adamya Singh <adamya45@gmail.com>
2023-07-05 01:36:00 +0530

    displayed chat rooms

--- src/context/rooms.context.js
+++ src/context/rooms.context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { database } from "../misc/firebase";
 import { transformToArrWithId } from "../misc/helpers";
 
@@ -20,5 +20,9 @@ export const RoomsProvider = ({children}) => {
         };
     },[]);
 
-    return <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+    return (
+    <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+    );
 }
+
+export const useRooms = () => useContext(RoomsContext);
