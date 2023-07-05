036799c89bbf9aa2787a58b6c4d7d551dd676119
Adamya Singh <adamya45@gmail.com>
2023-07-05 20:07:56 +0530

    create current room context with useContextSelector

--- src/pages/home/Chat.js
+++ src/pages/home/Chat.js
@@ -6,6 +6,7 @@ import ChatTop from '../../components/chat-window/top';
 import ChatBottom from '../../components/chat-window/bottom';
 import Messages from '../../components/chat-window/messages';
 import { useRooms } from '../../context/rooms.context';
+import { CurrentRoomProvider } from '../../context/current-room.context';
 
 
 const Chat = () => {
@@ -23,8 +24,14 @@ const Chat = () => {
         return <h6 className='tect-center mt-page'>Chat {chatId} not found</h6>
     }
 
+    const {name,description} = currentRoom;
+
+    const currentRoomData = {
+        name,description
+    };
+
   return ( 
-  <>
+  <CurrentRoomProvider data={currentRoomData}>
     <div className='chat-top'>
         <ChatTop />
     </div>
@@ -34,7 +41,7 @@ const Chat = () => {
     <div className='chat-bottom'>
         <ChatBottom />
     </div>
-  </>
+  </CurrentRoomProvider>
   )
 }
 
