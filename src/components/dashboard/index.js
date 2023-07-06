0e2e67b2b033eda6bc5a0e7c1f164813d2651bfa
Adamya Singh <adamya45@gmail.com>
2023-07-06 16:24:39 +0530

    update all user refrences if data is updated

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,55 +1,56 @@
-import React from 'react'
+import React from 'react';
 import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import EditableInput from '../EditableInput';
 import { database } from '../../misc/firebase';
 import ProviderBlock from './ProviderBlock';
 import AvatarUploadBtn from './AvatarUploadBtn';
+import { getUserUpdates } from '../../misc/helpers';
 
-const Dashboard = ({onSignOut}) => {
-
+const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
   const onSave = async newData => {
 
-    const userNicknaeRef = database.ref(`/profiles/${profile.uid}`).child('name');
-
     try {
-      await userNicknaeRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
 
       Alert.success('Nickname has been updated', 4000);
-
     } catch (err) {
-      Alert.error(err.message, 4000);      
+      Alert.error(err.message, 4000);
     }
-
-  }
+  };
 
   return (
     <>
-    <Drawer.Header>
-      <Drawer.Title>
-        Dashboard
-      </Drawer.Title>
-    </Drawer.Header>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
 
-    <Drawer.Body>
-      <h3>Hello, {profile.name}</h3>
-      <ProviderBlock />
-      <Divider />
-      <EditableInput
-        name="nicknake"
-        initialValue = {profile.name}
-        onSave = {onSave}
-        label = {<h6 className='mb-2'>Nickname</h6>}
+      <Drawer.Body>
+        <h3>Hello, {profile.name}</h3>
+        <ProviderBlock />
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
         />
-        <AvatarUploadBtn  />
-    </Drawer.Body>
+        <AvatarUploadBtn />
+      </Drawer.Body>
 
-    <Drawer.Footer>
-      <Button block color='red' onClick={onSignOut}>
-        Sign Out
-      </Button>
-    </Drawer.Footer>
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign Out
+        </Button>
+      </Drawer.Footer>
     </>
   );
 };
