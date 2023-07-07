025087549cd7bfaa09c497035bd892e52526dbbb
Adamya Singh <adamya45@gmail.com>
2023-07-07 18:39:58 +0530

    added button to grand or revoke admin access

--- src/components/chat-window/messages/ProfileInfoBtnModal.js
+++ src/components/chat-window/messages/ProfileInfoBtnModal.js
@@ -3,7 +3,7 @@ import { useModalState } from '../../../misc/custom-hooks';
 import { Button, Modal } from 'rsuite';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
 
-const ProfileInfoBtnModal = ({ profile, ...btnProps }) => {
+const ProfileInfoBtnModal = ({ profile, children, ...btnProps }) => {
   const { isOpen, close, open } = useModalState();
 
   const { name, avatar, createdAt } = profile;
@@ -34,6 +34,7 @@ const ProfileInfoBtnModal = ({ profile, ...btnProps }) => {
         </Modal.Body>
 
         <Modal.Footer>
+          {children}
           <Button block onClick={close}>
             Close
           </Button>
